a2f462a30334b455db39b32baf2a6b9da25396ea
Joselarrahondo <jlarraho@gmail.com>
2023-01-05 17:44:00 -0600

    Created test cases and conjur item special case

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,62 +1,78 @@
-function Item(name, sell_in, quality) {
+class Item {
+  constructor(name, sell_in, quality){
   this.name = name;
   this.sell_in = sell_in;
   this.quality = quality;
+  }
 }
 
-var items = []
-
-items.push(new Item('+5 Dexterity Vest', 10, 20));
-items.push(new Item('Aged Brie', 2, 0));
-items.push(new Item('Elixir of the Mongoose', 5, 7));
-items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
-
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
+class CheeseItem extends Item {
+  updateQuality(){
+    this.sell_in--
+    if(this.quality < 50){
+      this.quality++;
     }
   }
 }
+class BackStagePass extends Item{
+  updateQuality(){
+    
+    if(this.sell_in <= 10 && this.sell_in > 5) {
+      this.quality += 2;
+    } else if(this.sell_in <= 5 && this.sell_in > 0) {
+      this.quality +=3
+    } else if(this.sell_in < 0) {
+      this.quality = 0;
+    }else {
+      this.quality++;
+    }
+    this.sell_in--;
+  }
+}
+class Legendary extends Item{
+  updateQuality(){}
+    
+
+  }
+class BasicItem extends Item {
+  updateQuality() {
+    if(this.quality > 0){
+    if(this.sell_in < 0){
+      this.quality -= 2
+    } else{
+      this.quality--
+    }
+  }
+      this.sell_in--;
+  }
+}
+
+class Twiceasfast extends Item {
+  updateQuality(){
+    if(this.sell_in > 1){
+      this.quality -= 2
+    };
+    if(this.sell_in <= 1){
+      this.quality = 0 
+    };
+      this.sell_in--;
+  };
+};
+
+var items = []
+
+items.push(new BasicItem('+5 Dexterity Vest', 10, 20));
+items.push(new CheeseItem('Aged Brie', 2, 0));
+items.push(new BasicItem('Elixir of the Mongoose', 5, 7));
+items.push(new Legendary('Sulfuras, Hand of Ragnaros', 0, 80));
+items.push(new BackStagePass('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+items.push(new Twiceasfast('Conjured Mana Cake', 3, 6));
+
+function update_quality() {
+for (let item of items) {
+  item.updateQuality(); 
+}
+}
+
+
+
